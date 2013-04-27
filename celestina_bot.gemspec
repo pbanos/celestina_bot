df518298f9803228fb5e8748ab12df54a7ba4c16
Pablo Baños López <pbl1986@gmail.com>
2013-04-27 22:12:40 +0200

    Implements celestina_bot daemon

--- celestina_bot.gemspec
+++ celestina_bot.gemspec
@@ -21,4 +21,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
   spec.add_dependency 'grackle', '~> 0.3.0'
+  spec.add_dependency 'daemons', '~> 1.1.9'
 end
--- lib/celestina_bot.rb
+++ lib/celestina_bot.rb
@@ -4,5 +4,18 @@ require "celestina_bot/twitter"
 require 'celestina_bot/match'
 
 module CelestinaBot
-  # Your code goes here...
+	def self.work
+		match_maker = CelestinaBot::Match::Maker.new
+		loop do
+			puts "Making a match..."
+			begin
+				match = match_maker.make_match
+				puts "Match done: #{[match.calixto, match.melibea].collect(&:screen_name).inspect}"
+			rescue Grackle::TwitterError => e
+				$stderr.puts e.message
+			end
+			puts "Back to sleep"
+			sleep 60
+		end
+	end
 end
