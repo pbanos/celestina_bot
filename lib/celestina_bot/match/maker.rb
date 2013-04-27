3b4da2024049835332187411b666850aa217bca0
Pablo Baños López <pbl1986@gmail.com>
2013-04-27 18:25:00 +0200

    CelestinaBot::Match::Maker is now able to introduce made matches

--- lib/celestina_bot/match/maker.rb
+++ lib/celestina_bot/match/maker.rb
@@ -2,6 +2,13 @@ require 'celestina_bot/twitter'
 
 class CelestinaBot::Match::Maker
 
+	MATCH_INTRODUCTIONS = if (configured_introductions = CelestinaBot::Config['match_introductions']) and configured_introductions.is_a?(Array)
+		configured_introductions.collect(&:freeze)
+	else
+		$stderr.puts "Could not load match introductions from configuration"
+		exit(4)
+	end
+
 	attr_reader :strategy, :client
 
 	def initialize(strategy=nil)
@@ -11,6 +18,13 @@ class CelestinaBot::Match::Maker
 
 	def make_match
 		strategy.make_match(@client)
+	end
+
+	def introduce(match)
+		introduction = MATCH_INTRODUCTIONS.sample.
+			gsub('%{calixto}', "@#{match.calixto.screen_name}").
+				gsub('%{melibea}', "@#{match.melibea.screen_name}")
+		@client.post 'statuses/update', :status => introduction
 	end
 end
 
